2b1ac3ec0f61b6619961651f4187b0894d0b63e4
fanyspenda <fanyervansyah.9c@gmail.com>
2020-04-10 06:24:31 +0700

    add local storage to keep data when page reload

--- src/contexts/bookContext.js
+++ src/contexts/bookContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import uuid from "uuid/v1";
 import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 const BookContextProvider = (props) => {
-	const [books, dispatch] = useReducer(bookReducer, []);
+	const [books, dispatch] = useReducer(bookReducer, [], () => {
+		const result = localStorage.getItem("books");
+		return result ? JSON.parse(result) : [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem("books", JSON.stringify(books));
+	}, [books]);
 
 	return (
 		<BookContext.Provider value={{ books, dispatch }}>
